19aa25b0461749b8ea9eb52c5686d36a16e97622
sykg1204yuki <y.yuki040507@gmail.com>
2021-08-23 00:19:58 +0000

    コメント一覧表示修正

--- app/controllers/post_comments_controller.rb
+++ app/controllers/post_comments_controller.rb
@@ -1,32 +1,32 @@
 class PostCommentsController < ApplicationController
-  
+
   # コメント保存
   def create
-    post_image = PostImage.find(params[:post_image_id])
-    comment = PostComment.new(post_comment_params)
-    comment.user_id = current_user.id
-    comment.post_image_id = post_image.id
-    comment.save
-    redirect_to post_image_path(post_image.id)
-    
-  end 
-  
+    @post_image = PostImage.find(params[:post_image_id])
+    @comment = PostComment.new(post_comment_params)
+    @comment.user_id = current_user.id
+    @comment.post_image_id = @post_image.id
+    @comment.save
+    # redirect_to post_image_path(post_image.id)
+
+  end
+
   # コメント削除
   def destroy
-    post_image = PostImage.find(params[:post_image_id])
-    comment = post_image.post_comments.find(params[:id])
-    comment.destroy
-    redirect_to post_image_path(post_image.id)
-    
-  end 
-  
-  
+    @post_image = PostImage.find(params[:post_image_id])
+    @comment = @post_image.post_comments.find(params[:id])
+    @comment.destroy
+    # redirect_to post_image_path(post_image.id)
+
+  end
+
+
   # コメントのストロングパラメータ
   private
-  
+
   def post_comment_params
     params.require(:post_comment).permit(:comment)
-    
-  end 
-  
+
+  end
+
 end
