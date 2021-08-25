f8c9e504195961a26d5df6c3aa128bfcc4c30b3b
sykg1204yuki <y.yuki040507@gmail.com>
2021-08-25 02:40:55 +0000

    「いいね」した投稿一覧表示実装

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -45,6 +45,19 @@ class UsersController < ApplicationController
 
   end
 
+
+  # 「いいね」した投稿の一覧表示
+  def favorites
+    @user = User.find(params[:id])
+    favorites = Favorite.where(user_id: @user.id).pluck(:post_image_id)
+    @favorite_post_images = PostImage.find(favorites)
+
+  end
+
+
+
+
+
   # ユーザーのストロングパラメータ
   private
 
--- config/routes.rb
+++ config/routes.rb
@@ -3,9 +3,16 @@ Rails.application.routes.draw do
   devise_for :users
   root to: "homes#top"
   resources :users, only: [:index, :show, :edit, :update] do
+    
+    # フォロー・フォロワー機能ルーティング
     resource :relationships, only: [:create, :destroy]
     get :followings, on: :member
     get :followers, on: :member
+    
+    # 「いいね」した投稿検索機能ルーティング
+    member do
+      get :favorites
+    end 
   end
 
   resources :post_images do
