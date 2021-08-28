94fb5871a9a2ea42a39a71dc6429b1d957610cd3
sykg1204yuki <y.yuki040507@gmail.com>
2021-08-28 08:47:44 +0000

    view, route.rb修正

--- config/routes.rb
+++ config/routes.rb
@@ -1,32 +1,30 @@
 Rails.application.routes.draw do
 
-  devise_for :users
-  root to: "homes#top"
-  resources :users, only: [:index, :show, :edit, :update] do
-    resource :relationships, only: [:create, :destroy]
-    get :followings, on: :member
-    get :followers, on: :member
-    member do
-      get :favorites
-    end
-  end
-
-
-
-  resources :post_images do
-    resources :post_comments, only: [:create, :destroy]
-    resource :favorites, only: [:create, :destroy]
-    resource :discovers, only: [:create, :destroy]
-  end
-
-  resources :areas, only: [:index]
-  get '/map_areas' => 'areas#map_area'
-  get '/searches' => 'searches#search'
-  
-  
   scope '(:locale)', locale: /#{I18n.available_locales.map(&:to_s).join('|')}/ do
     # For details on the DSL available within this file, see
-    resources :resource_users, :resource_post_images, :resource_areas, :devise_for_users
+    devise_for :users
+    root to: "homes#top"
+    resources :users, only: [:index, :show, :edit, :update] do
+      resource :relationships, only: [:create, :destroy]
+      get :followings, on: :member
+      get :followers, on: :member
+      member do
+        get :favorites
+      end
+    end
+
+
+
+    resources :post_images do
+      resources :post_comments, only: [:create, :destroy]
+      resource :favorites, only: [:create, :destroy]
+      resource :discovers, only: [:create, :destroy]
+    end
+
+    resources :areas, only: [:index]
+    get '/map_areas' => 'areas#map_area'
+    get '/searches' => 'searches#search'
+    
   end
 
 
