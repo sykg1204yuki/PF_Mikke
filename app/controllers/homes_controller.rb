98fea18891bc66c13c6a8dea622def7cb36dcef0
sykg1204yuki <y.yuki040507@gmail.com>
2021-09-30 01:45:30 +0000

    ヘッダー修正

--- app/controllers/homes_controller.rb
+++ app/controllers/homes_controller.rb
@@ -1,6 +1,14 @@
 class HomesController < ApplicationController
+  before_action :signed_in
   
   def top 
+  end 
+  
+  def signed_in
+    if user_signed_in?
+      redirect_to user_path(current_user.id) 
+      
+    end
   end 
   
 end
